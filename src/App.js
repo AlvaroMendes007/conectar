bf088b6a53d6c4f1beafcb43a3b165403a9e8f79
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-08-28 17:14:17 -0300

    add Styles & map

--- src/App.js
+++ src/App.js
@@ -22,42 +22,53 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const datas  = [{
+  image: "/static/images/grid/complex.jpg",
+  alt: "complex",
+  name: "Product Name",
+  description: "Description",
+  price: "19,00"
+}];
+
 function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  ID: 1030114
-                </Typography>
+
+        {datas.map((data) => {
+          return (
+            <Grid container spacing={2}>
+            <Grid item>
+              <ButtonBase className={classes.image}>
+                <img className={classes.img} alt={data.alt} src={data.image} />
+              </ButtonBase>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom variant="subtitle1">
+                    {data.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {data.description}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography align="right" variant="body2" style={{ cursor: 'pointer' }}>
+                    Remove
+                  </Typography>
+                </Grid>
               </Grid>
               <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
+                <Typography variant="subtitle1">R${data.price}</Typography>
               </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">$19.00</Typography>
-            </Grid>
           </Grid>
-        </Grid>
+          )
+        })}
+
       </Paper>
     </div>
   );
