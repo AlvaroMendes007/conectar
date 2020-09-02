c5482a1a79e69ffa80197cc6192c49a1961881c4
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-09-02 12:51:42 -0300

    add Product page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Categoria from './pages/categories';
+// import Categoria from './pages/categories';
+import Produto from './pages/produtos';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Nav />
-    <Categoria />
+    <Produto />
+    {/* <Categoria /> */}
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/pages/produtos/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
+
+export default function ImgMediaCard() {
+    const [datas, setData] = useState([]);
+    // const [nome, setNome] = useState(null);
+    // const [imagem, setImagem] = useState(null);
+
+    const url = "http://localhost:3001/produtos";
+
+    useEffect(() => {
+        fetch(url)
+            .then((results) => results.json())
+            .then((data) => {
+                setData(data)
+            })
+    })
+    const classes = useStyles();
+
+    return (
+        <>
+            {datas.map((data) => {
+
+                return (
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt={data.image}
+                                height="140"
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                title={data.name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {data.name}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>)
+            })}
+        </>
+    );
+}
