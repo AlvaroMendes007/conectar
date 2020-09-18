81268206dc891429de46c58d13f84a081134a263
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-09-18 17:16:47 -0300

    custom endpoint

---
+++ BackEnd/api/src/main/java/com/conectar/api/entities/Product.java
@@ -0,0 +1,102 @@
+package com.conectar.api.entities;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String image;
+	private String name;
+	private String description;
+	private float price;
+	private int categoriaId;
+	
+	public Product() {
+		
+	}
+
+	public Product(Long id, String image, String name, String description, float price, int categoriaId) {
+		super();
+		this.id = id;
+		this.image = image;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.categoriaId = categoriaId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+	
+}
---
+++ BackEnd/api/src/main/java/com/conectar/api/resources/ProductResource.java
@@ -0,0 +1,38 @@
+package com.conectar.api.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conectar.api.entities.Product;
+
+@RestController
+public class ProductResource {
+	
+	@RequestMapping(value = "/produtos/{categoriaId}")
+	public ResponseEntity <List<Product>> findAll(@PathVariable("categoriaId") int categorieId){
+		
+		List<Product> produtos = new ArrayList();
+				
+		Product product = new Product(1L, "img", "Alface", "descr", 19, 1);
+		Product product1 = new Product(2L, "img", "Tomate", "descr", 19, 1);
+		Product product3 = new Product(3L, "img", "Cenoura", "descr", 19, 2);
+		Product product4 = new Product(4L, "img", "Batata", "descr", 19, 2);
+		Product product5 = new Product(5L, "img", "Pêra", "descr", 19, 3);
+		
+		produtos.add(product);
+		produtos.add(product1);
+		produtos.add(product3);
+		produtos.add(product4);
+		produtos.add(product5);
+		
+		produtos.removeIf(produto -> produto.getCategoriaId() != categorieId);
+		
+		return ResponseEntity.ok().body(produtos);
+	}
+}
